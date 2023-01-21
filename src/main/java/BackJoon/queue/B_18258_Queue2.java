35f568ef37344df68579e3f406fcb18ef3bc1f51
beetrbgus <devbeetrb0214@gmail.com>
2023-01-21 21:05:28 +0900

    feat - 23.01.20 문제 풀이 수정
    백준 문제
    - B_18258_Queue2 : 큐2
      - 시간초과 나는 이슈 해결
      - LinkedList 방식말고, int[] 사용

--- src/main/java/BackJoon/queue/B_18258_Queue2.java
+++ src/main/java/BackJoon/queue/B_18258_Queue2.java
@@ -3,7 +3,6 @@ package BackJoon.queue;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 /**
@@ -11,59 +10,71 @@ import java.util.StringTokenizer;
  *
  */
 public class B_18258_Queue2 {
-
+	static int[] queue;
+	static int frontIndex = 0;
+	static int backIndex = 0;
+	static int queueSize = 0;
 	public static void main(String args[]) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
+		queue = new int[N];
 		StringTokenizer st;
-		Queue que = new Queue();
 
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 
 			switch (st.nextToken()) {
-				case "push" : que.push(Integer.parseInt(st.nextToken()));
+				case "push" : push(Integer.parseInt(st.nextToken()));
+					break;
 				case "pop" :
-					System.out.println(que.pop());
+					sb.append(pop()).append('\n');
 					break;
 				case "size" :
-					System.out.println(que.size());
+					sb.append(size()).append('\n');
 					break;
 				case "empty" :
-					System.out.println(que.empty());
-
+					sb.append(empty()).append('\n');
 					break;
 				case "front" :
-					System.out.println(que.front());
+					sb.append(front()).append('\n');
 					break;
 				case "back" :
-					System.out.println(que.back());
+					sb.append(back()).append('\n');
 					break;
 			}
 		}
+		System.out.println(sb);
 	}
 
-	static class Queue {
-		LinkedList<Integer> queue = new LinkedList<>();
-
-		public void push(int num)  {
-			queue.offer(num);
+	public static void push(int num)  {
+		queue[backIndex] = num;
+		backIndex++;
+		queueSize++;
+	}
+	public static int pop()  {
+		int isEmpty = empty();
+		if(isEmpty == 0) {
+			int num = queue[frontIndex];
+			frontIndex++;
+			queueSize--;
+			return num;
+		} else {
+			return -1;
 		}
-		public int pop()  {
-			return !queue.isEmpty()? queue.pop() : -1;
-		}
-		public int size()  {
-			return queue.size();
-		}
-		public int empty() {
-			return queue.isEmpty()? 1 : 0;
-		}
-		public int front() {
-			return !queue.isEmpty()? queue.getFirst() : -1;
-		}
-		public int back() {
-			return !queue.isEmpty() ? queue.getLast() : -1;
-		}
+	}
+	public static int size()  {
+		return queueSize;
+	}
+	public static int empty() {
+		return queueSize > 0 ? 0 : 1;
+	}
+	public static int front() {
+		return empty() == 0 ? queue[frontIndex]: -1;
+	}
+	public static int back() {
+		return empty() == 0 ? queue[backIndex - 1]: -1;
 	}
 }
 
