e358a1bd77c490f79412afc7fd5df4e3ea402159
beetrbgus <devbeetrb0214@gmail.com>
2023-02-14 13:01:28 +0900

    feat - 23.02.14 문제 주석 추가
    프로그래머스 문제
    - lv0 : 모스부호
      - 주석 추가
      - for문 안의 알파벳의 char형 int 명시적 형변환을 삭제 : 그래도 int랑 더할 시 계산 잘 됨.

--- src/main/java/Programmers/lv0/P_120838_모스부호.java
+++ src/main/java/Programmers/lv0/P_120838_모스부호.java
@@ -8,19 +8,23 @@ import java.util.StringTokenizer;
 public class P_120838_모스부호 {
 
     public static String solution(String letter) {
+        // 모스부호 목록. 규칙이 없기 때문에 하드코딩으로 작성
         String[] morse = {
                 ".-","-...","-.-.","-..",".","..-.",
                 "--.","....","..",".---","-.-",".-..",
                 "--","-.","---",".--.","--.-",".-.",
                 "...","-","..-","...-",".--","-..-",
                 "-.--","--.."};
-        // 공백으로 구분
+        // letter를 공백으로 구분
         StringTokenizer st = new StringTokenizer(letter);
+
         StringBuilder sb = new StringBuilder();
+        // 토큰이 존재하지 않을 때까지 반복 = 배열의 끝까지 반복
         while(st.hasMoreTokens()) {
             String morseAlphabet = st.nextToken();
             for (int i = 0; i < morse.length; i++) {
-                char alphabet = (char)((int)'a' + i);
+                // 모스부호의 알파벳을 구함
+                char alphabet = (char)('a' + i);
                 if(morse[i].equals(morseAlphabet)) {
                     sb.append(alphabet);
                     break;
@@ -32,5 +36,6 @@ public class P_120838_모스부호 {
     public static void main(String[] args) {
         String letter = ".... . .-.. .-.. ---";
         System.out.println(solution(letter));
+        System.out.println((char)('a' + 1));
     }
 }
