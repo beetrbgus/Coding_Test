417df6d01693cad1fa0a76655df35ea2bc9f3d63
beetrbgus <devbeetrb0214@gmail.com>
2023-03-09 16:46:26 +0900

    style - 23.03.09 코드 수정 - 불필요한 if 분기 제거

--- src/main/java/Programmers/lv1/P_12928_약수의_합.java
+++ src/main/java/Programmers/lv1/P_12928_약수의_합.java
@@ -12,18 +12,15 @@ public class P_12928_약수의_합 {
      */
     public static int solution(int n) {
         int answer = 0;
-        if(n==0) return 0;
 
-        for (int i = 1; i < n/2; i++) {
-            if( n % i == 0) {
-                answer += i + (n / i);
-            }
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) answer += i;
         }
-        return answer;
+        return answer + n;
     }
 
     public static void main(String[] args) {
-        int n  = 12;
+        int n  = 16;
         int n2 = 5;
         int result1 = solution(n);
         int result2 = solution(n2);
