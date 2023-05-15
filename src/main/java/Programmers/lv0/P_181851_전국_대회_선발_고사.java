4de5891c1a06a46bb9785b88a3f81309be985644
beetrbgus <devbeetrb0214@gmail.com>
2023-05-15 23:58:05 +0900

    feat - 23.05.15 프로그래머스 알고리즘 추가
    알고리즘 문제
    - lv0:
      - 전국 대회 선발 고사

---
+++ src/main/java/Programmers/lv0/P_181851_전국_대회_선발_고사.java
@@ -0,0 +1,36 @@
+package Programmers.lv0;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *<a href="https://school.programmers.co.kr/learn/courses/30/lessons/181851">전국 대회 선발 고사</a>
+ */
+public class P_181851_전국_대회_선발_고사 {
+    public static int solution(int[] rank, boolean[] attendance) {
+        int answer = 0;
+        Map<Integer, Integer> rankMap = new HashMap<>();
+        for (int i = 0; i < attendance.length; i++) {
+            if(attendance[i]) {
+                rankMap.put(rank[i], i);
+            }
+        }
+        int plus = 10000;
+        int numCount = 0;
+        for (Integer az: rankMap.keySet()) {
+            Integer index = rankMap.get(az);
+            if(numCount < 3) {
+                answer += plus * index;
+                plus /= 100;
+            }
+            else break;
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        int[] rank = {3, 7, 2, 5, 4, 6, 1};
+        boolean[] attendance = {false, true, true, true, true, false, false};
+        int result = solution(rank, attendance);
+        System.out.println(result);
+    }
+}
