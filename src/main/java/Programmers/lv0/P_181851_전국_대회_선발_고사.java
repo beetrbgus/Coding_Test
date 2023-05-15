b9c09f304eab586edb76d1a01b73d45a83ce57df
beetrbgus <devbeetrb0214@gmail.com>
2023-05-16 00:00:56 +0900

    feat - 23.05.15 프로그래머스 알고리즘 추가
    알고리즘 문제
    - lv0:
      - 전국 대회 선발 고사
        - HashMap은 해시값이 충돌나서 실패가 뜨는 것 같음. TreeMap으로 변경

--- src/main/java/Programmers/lv0/P_181851_전국_대회_선발_고사.java
+++ src/main/java/Programmers/lv0/P_181851_전국_대회_선발_고사.java
@@ -1,7 +1,8 @@
 package Programmers.lv0;
 
-import java.util.HashMap;
+
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  *<a href="https://school.programmers.co.kr/learn/courses/30/lessons/181851">전국 대회 선발 고사</a>
@@ -9,7 +10,7 @@ import java.util.Map;
 public class P_181851_전국_대회_선발_고사 {
     public static int solution(int[] rank, boolean[] attendance) {
         int answer = 0;
-        Map<Integer, Integer> rankMap = new HashMap<>();
+        Map<Integer, Integer> rankMap = new TreeMap<>();
         for (int i = 0; i < attendance.length; i++) {
             if(attendance[i]) {
                 rankMap.put(rank[i], i);
